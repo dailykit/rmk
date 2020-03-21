912c3b242ec188201cebea166bf3928b26f3d171
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2020-03-22 00:34:45 +0530

    feat: Onboarding flow

--- pages/onboarding.js
+++ pages/onboarding.js
@@ -1,22 +1,33 @@
+import Router from "next/router";
+
 import { Input, Button } from "../components";
-import { validator } from "../utils";
 
 const reducer = (state, action) => {
   switch (action.type) {
     case "EMAIL":
       return {
         ...state,
-        email: {
-          value: action.payload,
-          error: null
-        }
+        email: action.payload.value
       };
     case "PASSWORD":
       return {
         ...state,
-        password: {
-          value: action.payload,
-          error: null
+        password: action.payload.value
+      };
+    case "ADDRESS":
+      return {
+        ...state,
+        address: {
+          ...state.address,
+          [action.payload.name]: action.payload.value
+        }
+      };
+    case "CARD":
+      return {
+        ...state,
+        card: {
+          ...state.card,
+          [action.payload.name]: action.payload.value
         }
       };
     default:
@@ -25,20 +36,32 @@ const reducer = (state, action) => {
 };
 
 const Home = () => {
+  const [stage, setStage] = React.useState(1);
   const [state, dispatch] = React.useReducer(reducer, {
-    email: {
-      value: "",
-      error: null
+    email: "",
+    password: "",
+    address: {
+      line1: "",
+      line2: "",
+      zip: "",
+      city: ""
     },
-    password: {
-      value: "",
-      error: null
+    card: {
+      number: "",
+      expiryDate: "",
+      cvv: "",
+      name: ""
     }
   });
 
   const nextStage = e => {
     e.preventDefault();
-    console.log("yes");
+    if (stage !== 3) {
+      setStage(stage + 1);
+    } else {
+      console.log(state);
+      Router.push("/menu");
+    }
   };
 
   return (
@@ -50,16 +73,20 @@ const Home = () => {
           Already have an account? <span className="text-blue">LOGIN</span>
         </div>
         <h1 className="text-gray text-4xl font-bold mb-16">Sign Up</h1>
-        {/* Stage 1 */}
         <div className="w-3/4">
-          <form onSubmit={nextStage}>
+          {/* Stage 1 */}
+          <form onSubmit={nextStage} hidden={stage !== 1}>
             <div className="mb-4">
               <Input
                 type="email"
                 placeholder="email"
+                name="email"
                 value={state.email.value}
                 onChange={e =>
-                  dispatch({ type: "EMAIL", payload: e.target.value })
+                  dispatch({
+                    type: "EMAIL",
+                    payload: { value: e.target.value }
+                  })
                 }
                 required
                 validate={true}
@@ -67,9 +94,13 @@ const Home = () => {
               <Input
                 type="password"
                 placeholder="password"
+                name="password"
                 value={state.password.value}
                 onChange={e =>
-                  dispatch({ type: "PASSWORD", payload: e.target.value })
+                  dispatch({
+                    type: "PASSWORD",
+                    payload: { value: e.target.value }
+                  })
                 }
                 pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
                 title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters."
@@ -79,8 +110,151 @@ const Home = () => {
             </div>
             <Button>Get Started</Button>
           </form>
+          {/* Stage 2 */}
+          <form onSubmit={nextStage} hidden={stage !== 2}>
+            <div className="mb-4">
+              <Input
+                type="text"
+                placeholder="address line 1"
+                name="line1"
+                value={state.address.line1}
+                onChange={e =>
+                  dispatch({
+                    type: "ADDRESS",
+                    payload: { name: e.target.name, value: e.target.value }
+                  })
+                }
+                required
+                validate={true}
+              />
+              <Input
+                type="text"
+                placeholder="address line 2"
+                name="line2"
+                value={state.address.line2}
+                onChange={e =>
+                  dispatch({
+                    type: "ADDRESS",
+                    payload: { name: e.target.name, value: e.target.value }
+                  })
+                }
+              />
+              <div className="flex">
+                <div className="w-6/12 mr-4">
+                  <Input
+                    type="text"
+                    placeholder="zip code"
+                    name="zip"
+                    value={state.address.zip}
+                    onChange={e =>
+                      dispatch({
+                        type: "ADDRESS",
+                        payload: { name: e.target.name, value: e.target.value }
+                      })
+                    }
+                    pattern="(\d{5}([\-]\d{4})?)"
+                    title="Format: nnnnn or nnnnn-nnnn"
+                    required
+                    validate={true}
+                  />
+                </div>
+                <div className="w-full">
+                  <Input
+                    type="text"
+                    placeholder="city"
+                    name="city"
+                    value={state.address.city}
+                    onChange={e =>
+                      dispatch({
+                        type: "ADDRESS",
+                        payload: { name: e.target.name, value: e.target.value }
+                      })
+                    }
+                    required
+                    validate={true}
+                  />
+                </div>
+              </div>
+            </div>
+            <Button>Proceed</Button>
+          </form>
+          {/* Stage 3 */}
+          <form onSubmit={nextStage} hidden={stage !== 3}>
+            <div className="mb-4">
+              <Input
+                type="text"
+                placeholder="Card number"
+                name="number"
+                value={state.card.number}
+                onChange={e =>
+                  dispatch({
+                    type: "CARD",
+                    payload: { name: e.target.name, value: e.target.value }
+                  })
+                }
+                pattern="[0-9]{13,16}"
+                title="13 to 16 digit card number"
+                required
+                validate={true}
+              />
+              <div className="flex">
+                <div className="w-3/4 mr-4">
+                  <Input
+                    type="text"
+                    placeholder="Expiry date (mm/yyyy)"
+                    name="expiryDate"
+                    value={state.card.expiryDate}
+                    onChange={e =>
+                      dispatch({
+                        type: "CARD",
+                        payload: { name: e.target.name, value: e.target.value }
+                      })
+                    }
+                    pattern="(0[1-9]|1[012])/[0-9]{4}"
+                    title="Format: mm/yyyy"
+                    required
+                    validate={true}
+                  />
+                </div>
+                <div>
+                  <Input
+                    type="text"
+                    placeholder="CVV"
+                    name="cvv"
+                    value={state.card.cvv}
+                    onChange={e =>
+                      dispatch({
+                        type: "CARD",
+                        payload: { name: e.target.name, value: e.target.value }
+                      })
+                    }
+                    pattern="[0-9]{3}"
+                    title="Format: nnn"
+                    required
+                    validate={true}
+                  />
+                </div>
+              </div>
+              <Input
+                type="text"
+                placeholder="Name on card"
+                name="name"
+                value={state.card.name}
+                onChange={e =>
+                  dispatch({
+                    type: "CARD",
+                    payload: { name: e.target.name, value: e.target.value }
+                  })
+                }
+                pattern="[A-Z]"
+                title="Uppercase letter only"
+                required
+                validate={true}
+              />
+            </div>
+            <Button>Sign Up</Button>
+          </form>
         </div>
-        {/* Stage 2 */}
       </div>
     </div>
   );
