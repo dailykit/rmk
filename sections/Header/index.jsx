420c1dd514bb4df1ec4008644a9818fd1b7259c3
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2020-03-21 20:06:17 +0530

    feat: Dropdown

--- sections/Header/index.jsx
+++ sections/Header/index.jsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 
 const Header = () => {
+  const [isDropdownVisible, setIsDropdownVisible] = React.useState(false);
+
   return (
     <nav className="bg-white h-16 w-full flex flex-row items-center px-8 fixed">
       <div className="w-32">
@@ -25,14 +27,47 @@ const Header = () => {
         <li className="mx-8 h-full flex items-center cursor-pointer font-semibold text-gray hover:text-blue">
           Cart
         </li>
-        <li className="mx-8 h-full flex items-center cursor-pointer font-semibold">
+        <li
+          className="mx-8 h-full flex items-center cursor-pointer font-semibold relative"
+          onClick={() => setIsDropdownVisible(!isDropdownVisible)}
+        >
           <img
             src="/img/avatar.jpg"
             className="w-10 h-10 rounded-full"
             alt="Avatar"
           />
+          <ul
+            className="dropdown absolute list-none bg-white z-10 shadow w-56 rounded-lg"
+            hidden={!isDropdownVisible}
+          >
+            <li className="p-4 text-lg text-gray"> Mia Khalifa </li>
+            <li className="py-2 px-4">Account</li>
+            <li className="py-2 px-4">Orders</li>
+            <li className="py-2 px-4">Delivery addresses</li>
+            <li className="py-2 px-4">Payment Info</li>
+            <li className="py-2 px-4">Settings</li>
+          </ul>
         </li>
       </ul>
+      <style jsx>
+        {`
+          .dropdown {
+            top: 5rem;
+            right: -2rem;
+          }
+
+          .dropdown::before {
+            content: "";
+            position: absolute;
+            background: #fff;
+            height: 1rem;
+            width: 1rem;
+            transform: rotate(45deg);
+            right: 2.5rem;
+            top: -0.5rem;
+          }
+        `}
+      </style>
     </nav>
   );
 };
