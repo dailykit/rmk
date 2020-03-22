81ddeceb27d6a7af513b98f54cde256bae482179
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2020-03-23 01:22:48 +0530

    fix: Landing page

--- client/pages/index.js
+++ client/pages/index.js
@@ -17,17 +17,17 @@ const Home = () => {
       <React.Fragment>
          <div className="hero h-screen bg-bottom bg-cover p-40">
             <div className="w-6/12 flex flex-col">
-               <p className="text-white text-lg font-semibold">
+               <p className="text-white text-2xl font-semibold">
                   Your <span className="text-5xl">Local Restaurants</span>
                </p>
-               <p className="text-white text-lg font-semibold mb-12">
+               <p className="text-white text-2xl font-semibold mb-12">
                   are now serving
                   <span className="text-6xl text-blue-500">Meal Kits</span>
                </p>
-               <p className="bg-white text-xl text-black font-semibold p-2 mb-2">
+               <p className="text-xl text-white font-semibold p-2 mb-2">
                   Groceries Sold Out? Staring at a Lock Down? Don't fret.
                </p>
-               <p className="bg-black text-xl text-white font-semibold p-2 text-right mb-8">
+               <p className="text-xl text-blue-500 font-semibold p-2 text-right mb-8">
                   Restaurants near you have got you back.
                </p>
                <form onSubmit={submit}>
@@ -36,14 +36,16 @@ const Home = () => {
                         type="text"
                         placeholder="Enter your zip code"
                         name="zip"
-                        className="p-2"
+                        className="p-2 flex-1"
                         value={zip}
                         onChange={e => setZip(e.target.value)}
                         pattern="(\d{5}([\-]\d{4})?)"
                         title="Format: nnnnn or nnnnn-nnnn"
                         required
                      />
-                     <Button>GET STARTED</Button>
+                     <div className="w-1/4">
+                        <Button>GET STARTED</Button>
+                     </div>
                   </div>
                </form>
             </div>
