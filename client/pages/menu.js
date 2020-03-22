ac2579c6dcde3235df8eaab85886e705db339774
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2020-03-23 00:34:43 +0530

    feat: Date Picker and Cart

--- client/pages/menu.js
+++ client/pages/menu.js
@@ -91,6 +91,92 @@ const Restaurant = () => {
    )
 }
 
+const DatePicker = () => {
+   return (
+      <React.Fragment>
+         <div className="flex border h-16 mb-8">
+            <div className="w-16 h-16 flex justify-center items-center">
+               <span className="text-blue-500 font-semibold cursor-pointer">
+                  Mar
+               </span>
+            </div>
+            <ul className="list-none flex h-full">
+               <li className="h-full w-16 bg-blue-500 rounded-lg flex flex-col justify-center items center font-medium cursor-pointer">
+                  <span className="text-center text-white">1</span>
+                  <span className="text-center text-white text-xs">Mon</span>
+               </li>
+               <li className="h-full w-16 rounded-lg flex flex-col justify-center items center font-medium cursor-pointer">
+                  <span className="text-center">2</span>
+                  <span className="text-center text-gray-500 text-xs">Tue</span>
+               </li>
+               <li className="h-full w-16 rounded-lg flex flex-col justify-center items center font-medium cursor-pointer">
+                  <span className="text-center">3</span>
+                  <span className="text-center text-gray-500 text-xs">Wed</span>
+               </li>
+            </ul>
+         </div>
+         <style jsx>{``}</style>
+      </React.Fragment>
+   )
+}
+
+const Cart = () => {
+   return (
+      <React.Fragment>
+         <div className="bg-white p-2">
+            <div className="title border-gray-500 text-2xl font-semibold mb-8">
+               YOUR DAILY KITS
+            </div>
+            <div className="cart-content">
+               <div className="progress border"></div>
+               <div>
+                  <div>
+                     <h3 className="mb-4">Meals selected</h3>
+                     <div className="border border-gray-300 p-4 mb-16">
+                        <h2 className="mb-4 text-md font-medium">
+                           Asian Restaurant name
+                        </h2>
+                        <div className="lunch-tile bg-white p-2 shadow font-bold mb-2">
+                           English Lunch
+                        </div>
+                        <div className="dinner-tile bg-white p-2 shadow font-bold">
+                           English Dinner
+                        </div>
+                     </div>
+                  </div>
+                  <div className="font-medium mb-16">
+                     We're waiting for restaurant to confirm your order. Stay
+                     put!
+                  </div>
+                  <div className="font-medium">
+                     <small>Delivering at:</small>
+                     <h3>1271, S Indiana Avenue, Chicago, Illinois, 60001</h3>
+                     <span className="text-blue-500 cursor-pointer text-sm">
+                        Change address
+                     </span>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <style jsx>{`
+            .title {
+               border-bottom: 1px solid #ececec;
+            }
+            .cart-content {
+               display: grid;
+               grid-template-columns: 2rem 1fr;
+            }
+            .lunch-tile {
+               border-left: 4px solid #f6ad55;
+            }
+            .dinner-tile {
+               border-left: 4px solid #2a4365;
+            }
+         `}</style>
+      </React.Fragment>
+   )
+}
+
 const Home = () => {
    return (
       <Layout>
@@ -99,11 +185,15 @@ const Home = () => {
                Your local restaurants are now serving Meal Kits
             </h1>
          </div>
+         <h2 className="font-semibold p-4">Hungerboard</h2>
+         <DatePicker />
          <div className="wrapper p-6">
             <div>
                <Restaurant />
             </div>
-            <div></div>
+            <div>
+               <Cart />
+            </div>
          </div>
          <style jsx>
             {`
@@ -113,6 +203,7 @@ const Home = () => {
                .wrapper {
                   display: grid;
                   grid-template-columns: 1fr 420px;
+                  grid-gap: 20px;
                }
             `}
          </style>
