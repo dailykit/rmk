9cc04f2cdec26f2843bf755020860643d6a34bce
Praveen Bisht <prvnbist@gmail.com>
2020-03-24 00:24:58 +0530

    feat: data-hub/apollo integration

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+// Apollo Client Imports
+import { ApolloProvider } from '@apollo/react-hooks'
+import { ApolloClient } from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { HttpLink } from 'apollo-link-http'
+import { ApolloLink } from 'apollo-link'
+
 import App from './App'
+import { AuthProvider } from './context/auth'
 
 import './global.css'
 import './styles.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const client = new ApolloClient({
+   link: ApolloLink.from([
+      new HttpLink({
+         uri: 'http://localhost:4000/graphql',
+      }),
+   ]),
+   cache: new InMemoryCache(),
+})
+
+ReactDOM.render(
+   <AuthProvider>
+      <ApolloProvider client={client}>
+         <App />
+      </ApolloProvider>
+   </AuthProvider>,
+   document.getElementById('root')
+)
