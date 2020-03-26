60f96cf4900e705984bb17672f8af81fa96f8430
Praveen Bisht <prvnbist@gmail.com>
2020-03-26 17:50:21 +0530

    feat: serve client on all paths

--- server/index.js
+++ server/index.js
@@ -12,6 +12,10 @@ app.use(express.static(path.join(__dirname, '/../client/build')))
 
 const PORT = process.env.PORT || 4000
 
+app.get('*', (req, res) => {
+   res.sendFile(path.join(__dirname + '/../client/build/index.html'))
+})
+
 app.listen(PORT, () => {
    console.log(`Server started on ${PORT}`)
 })
