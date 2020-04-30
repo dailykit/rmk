39f04040871dfa81470fa9de959a80655ef5ccf4
Praveen Bisht <prvnbist@gmail.com>
2020-04-30 13:42:48 +0530

    feat: responsive cards

--- client/src/sections/Section/index.jsx
+++ client/src/sections/Section/index.jsx
@@ -18,7 +18,7 @@ const Section = ({ type, defaultRecipe, recipes, onClick }) => {
             </span>
          </header>
 
-         <ul className="grid grid-cols-3 gap-4 overflow-x-auto">
+         <ul className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 gap-4 overflow-x-auto">
             {recipes.map((recipe, index) => (
                <RecipeCard
                   recipe={recipe}
