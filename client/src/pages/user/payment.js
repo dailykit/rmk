2424c9cc9fe0d84759e84ad9ad8f26ac2e6ef3f6
Praveen Bisht <prvnbist@gmail.com>
2020-05-01 14:04:07 +0530

    feat: payment cards ui

--- client/src/pages/user/payment.js
+++ client/src/pages/user/payment.js
@@ -8,32 +8,64 @@ const Payment = () => {
       <div>
          <Header />
          <ProfileLayout>
-            <h4 className="text-gray-700 font-semibold mb-8">Payment Info</h4>
             <div className="flex justify-between items-center text-md mb-4">
                <h3 className="text-lg">Payment Cards</h3>
                <span className="text-primary cursor-pointer">Add Card</span>
             </div>
-            <div className="flex flex-col">
-               <div className="bg-gray-300 h-32 w-full flex-col">
-                  <div className="text-md font-semibold h-16 flex items-center px-8">
-                     <span className="mr-16">XXXX XXXX XXXX 1234</span>
-                     <span className="mr-16">01/8989</span>
-                     <span className="mr-16">123</span>
-                  </div>
-                  <div className="text-md font-semibold h-16 flex items-center justify-between px-8">
-                     <span>Alex Pinto</span>
-                     <div>
-                        <span className="text-white bg-primary ml-8 cursor-pointer border-blue p-2">
-                           Default
-                        </span>
-                        <span className="text-gray-500 ml-8 cursor-pointer">
-                           Edit
-                        </span>
-                        <span className="text-gray-500 ml-8 cursor-pointer hover:text-red-500">
-                           Remove
-                        </span>
-                     </div>
-                  </div>
+            <div className="grid grid-cols-3 gap-4 mt-3">
+               <div className="border rounded-lg h-auto p-4">
+                  <header>
+                     <span className="mb-2 inline-block rounded border bg-teal-200 border-teal-300 px-2 text-teal-700">
+                        Default
+                     </span>
+                  </header>
+                  <main>
+                     <section className="mb-3 flex flex-1 text-gray-800">
+                        <h4 className="uppercase text-gray-600 tracking-wider font-medium">
+                           Name:
+                        </h4>
+                        <p>Alex Pinto</p>
+                     </section>
+                     <section className="mb-3 flex flex-1 text-gray-800">
+                        <h4 className="uppercase text-gray-600 tracking-wider font-medium">
+                           Number:
+                        </h4>
+                        <p>XXXX XXXX XXXX 1234</p>
+                     </section>
+                     <section className="flex flex-1 text-gray-800">
+                        <h4 className="uppercase text-gray-600 tracking-wider font-medium">
+                           Validity:
+                        </h4>
+                        <p>05/26</p>
+                     </section>
+                  </main>
+               </div>
+               <div className="border rounded-lg h-auto p-4">
+                  <header>
+                     <span className="mb-2 inline-block cursor-pointer rounded border border-primary  px-2 text-teal-700 hover:bg-primary hover:text-white">
+                        Make Default
+                     </span>
+                  </header>
+                  <main>
+                     <section className="mb-3 flex flex-1 text-gray-800">
+                        <h4 className="uppercase text-gray-600 tracking-wider font-medium">
+                           Name:
+                        </h4>
+                        <p>Alex Pinto</p>
+                     </section>
+                     <section className="mb-3 flex flex-1 text-gray-800">
+                        <h4 className="uppercase text-gray-600 tracking-wider font-medium">
+                           Number:
+                        </h4>
+                        <p>XXXX XXXX XXXX 5678</p>
+                     </section>
+                     <section className="flex flex-1 text-gray-800">
+                        <h4 className="uppercase text-gray-600 tracking-wider font-medium">
+                           Validity:
+                        </h4>
+                        <p>08/23</p>
+                     </section>
+                  </main>
                </div>
             </div>
          </ProfileLayout>
